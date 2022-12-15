1a13b8684eb716c9119824d9d523d3cf965b5247
alexbft <alexbft@github.com>
2022-12-15 14:41:10 +0100

    15_2 new union

--- 15_2.ts
+++ 15_2.ts
@@ -3,36 +3,23 @@ import { open } from 'node:fs/promises';
 type Interval = [number, number];
 type Point = [number, number];
 
-function join(existing: Interval[], aNew: Interval): Interval {
-  if (existing.length === 0) {
-    return aNew;
-  }
-  return [Math.min(aNew[0], existing[0][0]), Math.max(aNew[1], existing[existing.length - 1][1])];
-}
-
 function union(intervals: Interval[]): Interval[] {
-  const intervalUnion: Interval[] = [];
-  for (const [l, r] of intervals) {
-    let lIndex = intervalUnion.length;
-    for (let i = 0; i < intervalUnion.length; ++i) {
-      if (intervalUnion[i][1] >= l) {
-        lIndex = i;
-        break;
+  const temp = [...intervals];
+  temp.sort(([l0,], [l1,]) => l0 - l1);
+  const result: Interval[] = [];
+  for (const [l, r] of temp) {
+    if (result.length === 0) {
+      result.push([l, r]);
+    } else {
+      const [prevL, prevR] = result[result.length - 1];
+      if (prevR + 1 >= l) {
+        result[result.length - 1] = [prevL, Math.max(r, prevR)];
+      } else {
+        result.push([l, r]);
       }
     }
-    let rIndex = -1;
-    for (let i = intervalUnion.length - 1; i >= 0; --i) {
-      if (intervalUnion[i][0] <= r) {
-        rIndex = i;
-        break;
-      }
-    }
-    const inner = intervalUnion.slice(lIndex, rIndex + 1);
-    //console.log(`add: ${JSON.stringify(intervalUnion)} <- ${[l, r]}`);
-    const replace_with = join(inner, [l, r]);
-    intervalUnion.splice(lIndex, rIndex - lIndex + 1, replace_with);
   }
-  return intervalUnion;
+  return result;
 }
 
 // const inputFile = '15_test.txt';
